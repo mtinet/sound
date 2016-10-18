2247904d3b2b7dd81a93b3139b7351e8073a5f0f
Jason Juhyun Kim <mtinet@hanmail.net>
2016-10-18 13:37:32 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var song;
+
+function preload() {
+  song = loadSound('assets/Idiot Test - 90 fail.mp3');
+}
+
+function setup() {
+  
+  createCanvas(720, 200);
+  background(255,0,0);
+}
+
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
+  }
+}
